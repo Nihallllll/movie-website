402f6fcbc6e7a03c874b070a42d4430508d4f3d9
Nihallllll <nihal24rajak@gmail.com>
2025-03-18 10:05:58 +0530

    Added project files

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+const API_KEY = "a9a3e488480569df4e3e33d86f6c77b2";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
